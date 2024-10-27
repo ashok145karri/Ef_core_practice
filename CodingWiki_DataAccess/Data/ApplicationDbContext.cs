88547b8aa959fe7160028aac2f6e4ccef583d4c0
Ashok_karri <akarri@uworld.in>
2024-10-27 12:58:53 +0530

    Migrations

--- CodingWiki_DataAccess/Data/ApplicationDbContext.cs
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace CodingWiki_DataAccess.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Ef_Core_Practice;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(u => u.Price).HasPrecision(10, 5);
+
+            modelBuilder.Entity<Book>().HasData(
+
+                new Book { BookId = 1, Title = "SpiderMan", ISBN = "123ashok45", Price = 19.0m },
+                new Book { BookId = 2, Title = "SuperMan", ISBN = "45kohsa", Price = 55.99m }
+            );
+            var bookList = new Book[]
+            {
+                new Book { BookId = 3, Title = "Batman", ISBN = "8546lk", Price = 66.88m },
+                new Book { BookId = 4, Title = "Flash", ISBN = "82grs6", Price = 2.99m }
+            };
+            modelBuilder.Entity<Book>().HasData(bookList);
+        }
     }
+
+
 }
---
+++ CodingWiki_DataAccess/Migrations/20241027072655_seedDataInBookstable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodingWikiDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDataInBookstable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123ashok45", 19.0m, "SpiderMan" },
+                    { 2, "45kohsa", 55.99m, "SuperMan" },
+                    { 3, "8546lk", 66.88m, "Batman" },
+                    { 4, "82grs6", 2.99m, "Flash" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+        }
+    }
+}
