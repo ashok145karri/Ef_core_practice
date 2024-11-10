6ff65232168ff3b7b75ce776be545e9446dee763
Ashok_karri <akarri@uworld.in>
2024-11-10 17:36:55 +0530

    ef core crud operations

--- CodingWiki_Model/Models/Category.cs
+++ CodingWiki_Model/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace CodingWiki_Model.Models
         public int CategoryId { get; set; }
         [Column("Name")]
         [Required]
-        public string GenreName { get; set; }
+        public string CategoryName { get; set; }
         //public int DisplayOrder { get; set; }
     }
 }
